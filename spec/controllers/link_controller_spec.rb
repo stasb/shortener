8fcdb0ea6e875319acdb61bee349cad04a6364a1
Stas <stanislav.belkov@gmail.com>
2013-03-12 13:55:17 +1100

    Added model and controller specs.

--- spec/controllers/link_controller_spec.rb
+++ spec/controllers/link_controller_spec.rb
@@ -1,18 +1,30 @@
 require 'spec_helper'
 
-describe LinkController do
+describe LinksController do
 
-  describe "GET 'new'" do
-    it "returns http success" do
-      get 'new'
-      response.should be_success
+  let!(:http_link) { FactoryGirl.create(:link, store: 'http://something.com', alias: '123456') }
+  let!(:non_http_link) { FactoryGirl.create(:link, store: 'something.com', alias: 'abc123') }
+
+  describe "POST #create" do
+    it "shortens a link correctly" do
+      post :create, link: FactoryGirl.attributes_for(:link)
+      Link.last.alias.should_not be_nil
+    end
+    it "saves a link to the database" do
+      expect {
+        post :create, link: FactoryGirl.attributes_for(:link)
+      }.to change{Link.count}.by(1)
     end
   end
 
-  describe "GET 'create'" do
-    it "returns http success" do
-      get 'create'
-      response.should be_success
+  describe "#redirect" do
+    it "redirects to the correct link that contained http" do
+      get :redirect, alias: http_link.alias
+      expect(response).to redirect_to('http://something.com')
+    end
+    it "redirects to the correct link that doesn't contained http" do
+      get :redirect, alias: non_http_link.alias
+      expect(response).to redirect_to('http://something.com')
     end
   end
 
--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -1,5 +1,22 @@
 require 'spec_helper'
 
 describe Link do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it { should validate_uniqueness_of(:alias) }
+  it { should validate_presence_of(:store) }
+
+  let(:link1) { FactoryGirl.build(:link) }
+
+  it 'creates a link within the shorten method' do
+    Link.shorten(link1)
+    Link.last.should_not be_nil
+  end
+
+  it 'creates an alias' do
+    Link.shorten(link1)
+    Link.last.alias.should_not be_nil
+  end
+
+ it 'creates a unique alias'
+
 end
